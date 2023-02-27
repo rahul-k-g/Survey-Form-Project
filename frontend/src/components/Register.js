11f098001edc40dbf61cb67ea12aeb34dd30be3e
Rahul K G <kgrahul96@gmail.com>
2023-02-27 15:09:21 +0530

    created react app front end

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,22 @@
+import './Register.css';
+
+function Register() {
+  return (
+ <>
+ <section className='register-form'>
+   <div className='col-lg-12'>
+      <div className='col-lg-4'>
+         <h1>Welcome Page One line text Will be here</h1>
+         <p>Sign in to continue access pages</p>
+         <span>Already Have An Account</span>
+         <button>Sign in</button>
+      </div>
+      <div className='col-lg-8'></div>
+   </div>
+
+ </section>
+ </>
+  );
+}
+
+export default Register;
