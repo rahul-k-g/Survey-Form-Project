076ab952f9e474e0842252f15a91966f1a99f92a
MangeshaJadhav <mangeshjadhav987@gmail.com>
2023-02-28 10:09:04 +0530

    Sign and Register schema and page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // import Header from "./components/Header/header"
 import Register from "./components/Register/Register";
+import SignIn from "./components/Sign/SignIn";
 
 function App() {
   return (
     <div className="App">
-     <Register/>
+     {/* <Register/> */}
+     <SignIn/>
     </div>
   );
 }
