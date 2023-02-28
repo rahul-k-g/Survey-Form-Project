18804fabff3e2ae66b7b808e0f4c38e4a9abc9b2
MangeshaJadhav <mangeshjadhav987@gmail.com>
2023-02-28 19:23:14 +0530

    landing page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 // import Header from "./components/Header/header"
 import Register from "./components/Register/Register";
 import SignIn from "./components/Sign/SignIn";
+import Landing from "./components/Landing/Landing";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
 
@@ -10,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/" element={<SignIn />}></Route>
-        {/* <Route path="/landing" element={<LandingPage />}></Route> */}
+        <Route path="/landing" element={<Landing />}></Route>
         <Route path="*" element={<h1>404</h1>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import "./Landing.css";
+import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
+
+const Landing = () => {
+  return (
+    <div>
+
+      <Header />
+      <Sidebar />
+    
+    </div>
+  );
+};
+export default Landing;
