ce77d17806034c8bb858051fb4db029a675a11f6
Rahul K G <kgrahul96@gmail.com>
2023-03-01 22:43:32 +0530

    updated create questions and preview questions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,28 @@
-// import Header from "./components/Header/header"
 import Register from "./components/Register/Register";
 import CreateSurvey from "./components/Survey/CreateSurvey";
 import Surveylist from "./components/Surveylist/Surveylist";
 import SignIn from "./components/Sign/SignIn";
-import Landing from "./components/Landing/Landing";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CreateQuestions from "./components/Questions/CreateQuestions";
+import PreviewQuestions from "./components/Questions/PreviewQuestions";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return ( 
- 
+  return (
     <>
-   
-    {
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/" element={<SignIn />}></Route>
-        <Route path="/createsurvey" element={<CreateSurvey />}></Route>
-        <Route path="*" element={<h1>404</h1>}></Route>
-      </Routes>
-    </BrowserRouter>
-  </>
-   
-
+      {
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/" element={<SignIn />}></Route>
+            <Route path="/createsurvey" element={<CreateSurvey />}></Route>
+            <Route path="/surveylist" element={<Surveylist />}></Route>
+            <Route path="/createquestions" element={<CreateQuestions />}></Route>
+            <Route path="/previewquestions" element={<PreviewQuestions />}></Route>
+            <Route path="*" element={<h1>404</h1>}></Route>
+          </Routes>
+        </BrowserRouter>
+      }
+    </>
   );
 }
 
